5a7b31a4a77358467ca7f67c6f7a119766ab70c1
Vivien <vivien@MacBook-Pro-de-Vivien.local>
2022-10-25 15:27:45 +0200

    CSS Navbar

--- src/components/LayoutNavbar.tsx
+++ src/components/LayoutNavbar.tsx
@@ -5,7 +5,40 @@ import { Outlet, Link } from "react-router-dom";
 function LayoutNavbar() {
 
   return (
-    <div>
+    <>
+      <div className="nav">
+        <input type="checkbox" id="nav-check" />
+        <div className="nav-header">
+          <div className="nav-title">
+            Logo
+          </div>
+        </div>
+        <div className="nav-btn">
+          <label htmlFor="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+
+        <div className="nav-links">
+          {/* <a href="" target="">Link</a> */}
+          <Link to="/">Exercices</Link>
+          <Link to="/addExercice">Ajouter un Exercice</Link>
+          <Link to="/tags">Tags</Link>
+        </div>
+      </div>
+
+      <Outlet />
+    </>
+
+  );
+}
+
+export default LayoutNavbar;
+
+
+{/* <div>
       <nav className="navbar">
         <ul>
           <li>
@@ -21,8 +54,4 @@ function LayoutNavbar() {
       </nav>
       <hr />
       <Outlet />
-    </div>
-  );
-}
-
-export default LayoutNavbar;
+    </div> */}
